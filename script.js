a034e2903b5e17b7e6df0bc865c45fb972d101e9
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-08-26 17:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
 //your JS code here. If required.
+let Output = document.getElemntById('output');
+let Input = document.getElemntById('ip');
+
+const Submit = document.getElementById('btn');
+
+Submit.addEventListener("click", ValidateInput);
+
+// Consume Promises
+ValidateInput()
+.then(MultiplyWith2)
+.then(Subtract3)
+.then(DivideBy2)
+.then(Add10)
+.catch((err) => console.log(err.message));
+
+
+// Create Promises
+function ValidateInput(){
+	return new Promise((resolve, reject) => {
+		let InputValue = parseInt(Input.value.trim());
+		setTimeout(function(){
+			if(isNaN(InputValue)){
+				const err = new Error("Invalid Input!");
+				reject(err);
+			}else{
+				Output.innerText = `Result : ${InputValue}`;
+				resolve(InputValue);
+			}
+		}, 2000)
+	})
+}
+
+function MultiplyWith2(num){
+	return new Promise((resolve) => {
+		let product = num * 2;
+		setTimeout(function(){
+			Output.innerText = `Result : ${product}`
+			resolve(product)
+		}, 1000)
+	})
+}
+
+function Subtract3(num){
+	return new Promise((resolve) => {
+		let difference = num - 3;
+		setTimeout(function(){
+			Output.innerText = `Result : ${difference}`
+			resolve(difference)
+		}, 1000)
+	})
+}
+
+function DivideBy2(num){
+	return new Promise((resolve) => {
+		let divident = Math.floor(num/2);
+		setTimeout(function(){
+			Output.innerText = `Result : ${divident}`
+			resolve(divident)
+		}, 1000)
+	})
+}
+
+function Add10(num){
+	return new Promise((resolve) => {
+		let add = num + 10;
+		setTimeout(function(){
+			Output.innerText = `Result : ${add}`
+			resolve(add)
+		}, 1000)
+	})
+}
+
+
+
